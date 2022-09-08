db1d0465ce4b687017c2d8f227070ec1b737a8cc
Muhammad muayyat <100189331+Muhammadmuayyat123@users.noreply.github.com>
2022-09-08 13:56:51 +0800

    Add files via upload

---
+++ Latihan2.java
@@ -0,0 +1,24 @@
+package latihan2;
+public class Latihan2 {
+    public static void main(String[] args) {
+        String nama = "yayat";
+        byte umur = 20;
+        boolean pemahaman = false;
+        short tahun = 2002;
+        int tinggi = 170;
+        long jarak = 1902921092;
+        float ipk = 4.0f;
+        double kelas = 2.d;
+        char gol_darah = 'B';
+            System.out.println("Nama : " + nama);
+            System.out.println("umur : " + umur);
+            System.out.println("Ket.Memahami materi : "+ pemahaman);
+            System.out.println("Tahun Lahir : "+ tahun);
+            System.out.println("Tinggi badan : " + tinggi);
+            System.out.println("jarak Rumah dari kampus : "+ jarak);
+            System.out.println("ipk : "+ ipk);
+            System.out.println("Kelas : "+ kelas);
+            System.out.println("golongan darah : "+ gol_darah);
+    }
+    
+}

a875bf2adbeca0a8d6cdbd355af928041f6402f6
Muhammad muayyat <100189331+Muhammadmuayyat123@users.noreply.github.com>
2022-09-18 18:26:02 +0800

    Add files via upload

---
+++ Project6.java
@@ -0,0 +1,33 @@
+package project6;
+public class Project6 {
+    public static void main(String[] args) {
+        
+        // Operator assigment
+        
+        // Assignment operator perjumlahan 
+        int a = 2;
+        a += 23; // a = a + 10
+        System.out.println("nilai a = "+a);
+        
+        // Assignment operator pengurangan
+        int b = 10;
+        b -= 5; // b = b - 5
+        System.out.println("nilai b = "+b);
+        
+        // Assignment operator pembagian
+        int c = 10;
+        c /= 2; // c = c / 10
+        System.out.println("nilai c = "+c);
+        
+        // Assignment operator perkalian 
+        int d = 2;
+        d *= 10; // d = d x 10
+        System.out.println("nilai d = "+d);
+        
+        // Assignment operator perjumlahan 
+        int e = 2;
+        e %= 23; // e = e % 10
+        System.out.println("nilai e = "+e);
+    }
+    
+}
